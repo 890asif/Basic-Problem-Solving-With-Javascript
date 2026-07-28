2622e129c6878c7118e9d8457bc44376caa11af2
890asif <192508892+890asif@users.noreply.github.com>
2026-07-28 18:31:59 +0600

    product of digit

--- product.js
+++ product.js
@@ -1,3 +1,11 @@
-function productOfDigit(){
-    
+//product of digit
+function productOfDigit(num){
+    let str = num.toString();
+    let total = 1;
+    for(let i = 0; i < str.length; i++ ){
+    let digit = parseInt(str[i])
+    total = total * digit;
 }
+return total
+}
+console.log(productOfDigit(4040))

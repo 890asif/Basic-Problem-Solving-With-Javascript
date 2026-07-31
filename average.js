c5d7ae669d414b633f6bfe313727c32c7c433be8
890asif <192508892+890asif@users.noreply.github.com>
2026-07-31 23:47:09 +0600

    count multipalse of three

---
+++ average.js
@@ -0,0 +1,9 @@
+function averageOfArray(numbers){
+    
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i]
+    }
+    return sum / numbers.length;
+}
+console.log(averageOfArray([2, 4, 6]))

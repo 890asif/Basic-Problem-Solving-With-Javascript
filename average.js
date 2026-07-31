23a77c8688dcaa239acba6e49f8d5ad04d4ec51b
890asif <192508892+890asif@users.noreply.github.com>
2026-07-31 23:48:34 +0600

    average of array

--- average.js
+++ average.js
@@ -1,5 +1,7 @@
 function averageOfArray(numbers){
-    
+    if(!Array.isArray(numbers) || numbers === 0){
+        return "Invalid";
+    }
     let sum = 0;
     for(let i = 0; i < numbers.length; i++){
         sum += numbers[i]
